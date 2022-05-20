9a3a2a8a93a8264fed9770f383ff60c4a4057c94
yamenko <yamenko@yandex.ru>
2022-05-20 23:33:31 +0300

    Begin my first game

---
+++ Source/TankClassic/TankPawn.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "TankPawn.generated.h"
+
+UCLASS()
+class TANKCLASSIC_API ATankPawn : public APawn
+{
+	GENERATED_BODY()
+private:
+
+public:
+	// Конструктор класса (базовый)
+	ATankPawn();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+};
